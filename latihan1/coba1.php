e80fe57f15ea651dc3e1b84cdc56bfbdcbda40a9
fisabduh04 <fisabduh04@gmail.com>
2021-04-01 11:11:11 +0700

    tambah data

---
+++ latihan1/coba1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasisa TBK</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa TBK</h3>
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>No</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan </th>
+      <th>Aksi</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td><img src="img/abdul.JPG" alt=""></td>
+      <th>123</th>
+      <th>Mohammad Abdullah</th>
+      <th>fisabduh04@gmail.com</th>
+      <th>Teknik Bangunan Kapal </th>
+      <th><a href="">ubah</a>|<a href="">Hapus</a></th>
+    </tr>
+  </table>
+
+</body>
+
+</html>
